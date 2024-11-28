4eba37d9ec9bd63e6c0ca88ba805b9ea8eb0d8bb
K_Wang <k.wang@stu.xidian.edu.cn>
2024-11-29 00:26:52 +0800

    Add README

--- main/e-paper.c
+++ main/e-paper.c
@@ -1,6 +1,14 @@
+/* 
+ * @filename: e-paper.c
+ * @author:   k.wang
+ * @brief:    This is main function of e-paper app, includes creating each task
+ * @version:  1.0
+ * @note:     
+ */
 #include <stdio.h>
 
 void app_main(void)
 {
-
+  
 }
+
